edf098f687b517587fc8b198cb53cf0c2d01656a
Danilo Pasquariello <d.pasquariello@virtalis.com>
2022-08-25 22:42:12 +0100

    Implemented data persistence between sessions

--- Assets/Scripts/MyScripts/BestScoreManager.cs
+++ Assets/Scripts/MyScripts/BestScoreManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class BestScoreManager : MonoBehaviour
@@ -7,6 +8,8 @@ public class BestScoreManager : MonoBehaviour
     public static BestScoreManager Instance;
 
     public string ScoreName;
+    public string BestScoreName;
+    public int BestScorePoints;
 
     private void Awake()
     {
@@ -18,5 +21,36 @@ public class BestScoreManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public string ScoreName;
+        public string BestScoreName;
+        public int BestScorePoints;
+    }
+
+    public void SaveBestScore()
+    {
+        SaveData data = new SaveData();
+
+        data.ScoreName = ScoreName;
+        data.BestScoreName = BestScoreName;
+        data.BestScorePoints = BestScorePoints;
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "savefile.json", json);
+    }
+
+    public void LoadBestScore()
+    {
+        string path = Application.persistentDataPath + "savefile.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+        }
     }
 }
