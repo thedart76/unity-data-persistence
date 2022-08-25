b6ab62c311b8d1209ac91497fb3445c39ba9351e
Danilo Pasquariello <d.pasquariello@virtalis.com>
2022-08-25 20:45:18 +0100

    Implemented StartGame and QuitGame logic

---
+++ Assets/Scripts/MyScripts/MenuUIHandler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuUIHandler : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        if (EditorApplication.isPlaying)
+        {
+            EditorApplication.ExitPlaymode();
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
